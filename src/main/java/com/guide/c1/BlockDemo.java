ea16a45d2baeb4433989485ec467bed5d89a67cb
Franz Mejia <mejiafranz@gmail.com>
2024-01-20 23:52:44 -0400

    feat: add BlockDemo to review if block demo

---
+++ src/main/java/com/guide/c1/BlockDemo.java
@@ -0,0 +1,17 @@
+package com.guide.c1;
+
+public class BlockDemo {
+  public static void main(String[] args) {
+    double i, j, k;
+
+    i = 5;
+    j = 10;
+
+    if(i < j) {
+      System.out.println("i does not equal zero");
+      k = j / i;
+      System.out.println("j / i is " + k);
+    }
+
+  }
+}
